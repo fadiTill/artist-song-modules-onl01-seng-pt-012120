7e94aa1b0fa23489da88039c5a4aaeb738814090
fadimata tillman <fadi.tillman@gmail.com>
2020-02-24 22:21:38 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -29,11 +29,40 @@ require 'pry'
   #   self.all.count
   # end
 
-  def artist=(artist)
-    @artist = artist
+#   def artist=(artist)
+#     @artist = artist
+#   end
+#
+#   def to_param
+#     name.downcase.gsub(' ', '-')
+#   end
+# end
+class Song
+
+  extend Memorable::ClassMethods
+  extend Findable::ClassMethods
+  include Memorable::InstanceMethods
+  include Paramable::InstanceMethods
+
+  attr_accessor :name
+  attr_reader :artist
+
+  @@songs = []
+
+  def self.all
+    @@songs
   end
 
-  def to_param
-    name.downcase.gsub(' ', '-')
+  def artist=(artist)
+    @artist = artist
+    artist.add_song(self) unless artist.songs.include?(self)
   end
+
 end
+# //////////////////////////////////////////
+adele = Artist.new
+adele.name = "Adele"
+who_cares = Song.new
+who_cares.name = "Who Cares"
+adele.add_song(who_cares)
+Artist.find_by_name("Adele").name
